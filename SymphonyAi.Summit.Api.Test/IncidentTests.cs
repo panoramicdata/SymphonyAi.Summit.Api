f27659e0af83ea9fafbf004e2fb2b87e5d74d947
andrewhallmark <46025270+TheCakeMonster@users.noreply.github.com>
2023-02-14 15:28:15 +0000

    Test for setting a custom field - field and its group must have been defined through the UI before use

--- SymphonyAi.Summit.Api.Test/IncidentTests.cs
+++ SymphonyAi.Summit.Api.Test/IncidentTests.cs
@@ -69,4 +69,29 @@ public class IncidentTests : TestBase
 		response.Errors.Should().BeEmpty();
 		response.TicketNumber.Should().BeGreaterThan(0);
 	}
+
+	[Fact]
+	public async Task CreateIncident_GoodRequestWithCustomField_Succeeds()
+	{
+		// Load CreateOrUpdateIncidentRequest from file CreateIncidentRequest.json
+		var request = await LoadObjectFromJsonFile<CreateOrUpdateIncidentRequest>("CreateIncidentRequest", default);
+		request.CommonParameters.IncidentParamsJson.IncidentContainerJsonObj.Ticket.AssignedWorkGroupName = WorkgroupName;
+		request.CommonParameters.IncidentParamsJson.IncidentContainerJsonObj.Ticket.CallerEmailId = CallerEmailId;
+
+		var customField = new
+		{
+			GroupName = "Integration",
+			Name = "External Issue ID",
+			Value = "13579"
+		};
+		request.CommonParameters.IncidentParamsJson.IncidentContainerJsonObj.CustomFields.Add(customField);
+
+		request.ServiceName.Should().Be("IM_LogOrUpdateIncident");
+		request.CommonParameters.RequestType.Should().Be("RemoteCall");
+
+		var response = await SummitClient.Incidents.CreateOrUpdateIncidentAsync(request, CancellationToken.None);
+		response.Should().NotBeNull();
+		response.Errors.Should().BeEmpty();
+		response.TicketNumber.Should().BeGreaterThan(0);
+	}
 }
