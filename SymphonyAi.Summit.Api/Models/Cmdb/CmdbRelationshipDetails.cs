e7d3900a70bb1541e76361b3a29aa331f2db343c
David Bond <david.bond@panoramicdata.com>
2023-04-28 12:00:27 +0100

    Updated property names

--- SymphonyAi.Summit.Api/Models/Cmdb/CmdbRelationshipDetails.cs
+++ SymphonyAi.Summit.Api/Models/Cmdb/CmdbRelationshipDetails.cs
@@ -1,12 +1,27 @@
-﻿namespace SymphonyAi.Summit.Api.Models.Cmdb;
+﻿using System.Text.Json.Serialization;
+
+namespace SymphonyAi.Summit.Api.Models.Cmdb;
 
 public class CmdbRelationshipDetails
 {
+	[JsonPropertyName("InstanceName")]
 	public string InstanceName { get; set; } = string.Empty;
-	public string Configuration_Id { get; set; } = string.Empty;
-	public string Device_Host_Name { get; set; } = string.Empty;
-	public string Serial_No { get; set; } = string.Empty;
-	public string IPAddress { get; set; } = string.Empty;
+
+	[JsonPropertyName("Configuration_Id")]
+	public string ConfigurationId { get; set; } = string.Empty;
+
+	[JsonPropertyName("Device_Host_Name")]
+	public string DeviceHostName { get; set; } = string.Empty;
+
+	[JsonPropertyName("Serial_No")]
+	public string SerialNumber { get; set; } = string.Empty;
+
+	[JsonPropertyName("IPAddress")]
+	public string IpAddress { get; set; } = string.Empty;
+
+	[JsonPropertyName("CurrentPageIndex")]
 	public int CurrentPageIndex { get; set; }
+
+	[JsonPropertyName("PageSize")]
 	public int PageSize { get; set; }
 }
